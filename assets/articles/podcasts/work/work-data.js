b181775edf7842c70f99d76978bc0c88133cca3f
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-03-10 18:18:15 +0200

    Додав аудіо-подкаст

---
+++ assets/articles/podcasts/work/work-data.js
@@ -0,0 +1,9 @@
+const list = [
+    {
+      id: 1,
+      url: "./work.mp3",
+      author: "Подкаст про IT",
+      title: "Робота під час війни",
+      cover: "../player/podcast.png",
+    },
+  ];
