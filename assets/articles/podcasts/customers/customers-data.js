ccdd94d7a392f583948bf1f989fa143923bb3364
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-03-10 18:40:28 +0200

    Додав аудіо-подкаст

---
+++ assets/articles/podcasts/customers/customers-data.js
@@ -0,0 +1,9 @@
+const list = [
+    {
+      id: 1,
+      url: "./customers.mp3",
+      author: "Подкаст про IT",
+      title: "Токсичність з клієнтами",
+      cover: "../player/podcast.png",
+    },
+  ];
