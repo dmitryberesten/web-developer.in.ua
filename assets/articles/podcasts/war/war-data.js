b4e3e8648c6e99fdde246700d6f25cc2bf60b3a5
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-03-10 19:00:03 +0200

    Додав аудіо-подкаст

---
+++ assets/articles/podcasts/war/war-data.js
@@ -0,0 +1,9 @@
+const list = [
+    {
+      id: 1,
+      url: "./war.mp3",
+      author: "Подкаст про IT",
+      title: "Війна та продуктивність",
+      cover: "../player/podcast.png",
+    },
+  ];
