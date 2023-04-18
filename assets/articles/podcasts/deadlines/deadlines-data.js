55399e448c65e82e04219990883abbdff8c3fe68
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-04-19 02:15:01 +0300

    Додає 2 подкасти та 1 ютубера

---
+++ assets/articles/podcasts/deadlines/deadlines-data.js
@@ -0,0 +1,9 @@
+const list = [
+    {
+      id: 1,
+      url: "./deadlines.mp3",
+      author: "Подкаст про IT",
+      title: "Дедлайни і вигорання",
+      cover: "../player/podcast.png",
+    },
+  ];
---
+++ assets/articles/podcasts/start-it/start-it-data.js
@@ -0,0 +1,9 @@
+const list = [
+    {
+      id: 1,
+      url: "./start-it.mp3",
+      author: "Подкаст про IT",
+      title: "Як потрапили в ІТ",
+      cover: "../player/podcast.png",
+    },
+  ];
