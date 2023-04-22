48875835d0bdfa2c0dc5f83835179dffef8eb22d
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-04-22 16:15:55 +0300

    Додає спінер фактам

--- assets/generators/generator-facts/generator-facts.js
+++ assets/generators/generator-facts/generator-facts.js
@@ -10,6 +10,20 @@ function renderFact() {
   let factString = `<p class="fact"><b>${facts.fact}</b></p><p class="source">${facts.source}`;
 
   factContainer.innerHTML = factString;
+
+  let spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+
+  factContainer.classList.remove("show");
+
+  setTimeout(() => {
+    spinner.style.display = "none";
+    factContainer.classList.add("show");
+  }, 1000);
 }
 
-document.getElementById("loadFact").addEventListener("click", renderFact);
+document.getElementById("loadFact").addEventListener("click", function () {
+  renderFact();
+});
+
+window.addEventListener("load", renderFact);
