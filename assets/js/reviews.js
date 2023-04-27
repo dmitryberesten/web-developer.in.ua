868c72cdddf1d7d3f97bde0613b3ee5c310c61ef
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-04-27 18:24:48 +0300

    Update reviews.js

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -9,97 +9,97 @@ const reviews = [
   {
     name: "Iryna Shashko",
     position: "Junior Front-End developer",
-    photo: "../../images/rewievs/2.JPG",
+    photo: "../../images/rewievs/2.jpg",
     review:
       "Круто!!👍",
   },
   {
     name: "Svitlana Honchar",
     position: "Frontend Developer",
-    photo: "../../images/rewievs/3.JPG",
+    photo: "../../images/rewievs/3.jpg",
     review:
       "Вау, дуже крута і ідея, і робота💫",
   },
   {
     name: "Mariia Lytvyn",
     position: "Frontend Developer",
-    photo: "../../images/rewievs/4.JPG",
+    photo: "../../images/rewievs/4.jpg",
     review: "Крута робота!😉",
   },
   {
     name: "Olena Tsaruk",
     position: "Web Developer",
-    photo: "../../images/rewievs/5.JPG",
+    photo: "../../images/rewievs/5.jpg",
     review:
       "Це дивовижно! 😍 ",
   },
   {
     name: "Anton Bondar",
     position: "Frontend Developer/React Developer",
-    photo: "../../images/rewievs/6.JPG",
+    photo: "../../images/rewievs/6.jpg",
     review:
       "Круто!!!",
   },
   {
     name: "Olena Holubonkova",
     position: "junior software engineer",
-    photo: "../../images/rewievs/7.JPG",
+    photo: "../../images/rewievs/7.jpg",
     review:
       "Круто!!! Молодці!!",
   },
   {
     name: "Oleksii Zhmud",
     position: "PHP WEB Developer",
-    photo: "../../images/rewievs/8.JPG",
+    photo: "../../images/rewievs/8.jpg",
     review:
       "Молодці. Цікава ідея стосовно каталогу українських IT ютуберів!",
   },
   {
     name: "Anastasiia Hordieieva",
     position: "Web Developer",
-    photo: "../../images/rewievs/9.JPG",
+    photo: "../../images/rewievs/9.jpg",
     review:
       "Якісна робота, класна ідея!",
   },
   {
     name: "Serhii Natoilad",
     position: "Front-End Developer",
-    photo: "../../images/rewievs/10.JPG",
+    photo: "../../images/rewievs/10.jpg",
     review:
       "Ну прям вогонь! Дуже круто!",
   },
   {
     name: "Vadym Hrashchenko",
     position: "Full Stack Engineer",
-    photo: "../../images/rewievs/11.JPG",
+    photo: "../../images/rewievs/11.jpg",
     review:
       "that’s cool👍",
   },
   {
     name: "Viktoriia Serkiz",
     position: "Junior Front-end Developer (JS, React)",
-    photo: "../../images/rewievs/12.JPG",
+    photo: "../../images/rewievs/12.jpg",
     review:
       "Дуже крута ідея! Молодці)",
   },
   {
     name: "Volodymyr Tatarenkov",
     position: "Frontend-Developer",
-    photo: "../../images/rewievs/13.JPG",
+    photo: "../../images/rewievs/13.jpg",
     review:
       "АГОНЬ!!!",
   },
   {
     name: "Наталія Балюра",
     position: "Frontend Developer",
-    photo: "../../images/rewievs/14.JPG",
+    photo: "../../images/rewievs/14.jpg",
     review:
       "Дякую! Дуже круто!",
   },
   {
     name: "Evgeniya Vazhina (Danilova)",
     position: "HR/L&D",
-    photo: "../../images/rewievs/15.JPG",
+    photo: "../../images/rewievs/15.jpg",
     review:
       "Супер ідея! Дякую!",
   },
