6f21aef3187a3df2e24d599432096aabf53c16f2
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-04-29 00:45:08 +0300

    Додає відгуки

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -103,10 +103,136 @@ const reviews = [
     review:
       "Супер ідея! Дякую!",
   },
+  {
+    name: "Anton Nakonechnyi",
+    position: "Junior frontend",
+    photo: "../../images/rewievs/16.jpg",
+    review:
+      "Цікавий проект",
+  },
+  {
+    name: "Khrystyna Filipenko",
+    position: "Frontend Developer",
+    photo: "../../images/rewievs/17.jpg",
+    review:
+      "Круто! 👍",
+  },
+  {
+    name: "Rostyslav Bahniuk",
+    position: "Посада Accountant у Greenville",
+    photo: "../../images/rewievs/18.jpg",
+    review:
+      "Цікавий проект і саме головне українською. Так тримати. Дякую за те, що робите і поширюєте.",
+  },
+  {
+    name: "Volodymyr Skaskiv",
+    position: "ТОВ СПЕЦБУД-ПЛЮС",
+    photo: "../../images/rewievs/19.jpg",
+    review:
+      "Хороший проект, успіхів.",
+  },
+  {
+    name: "Kseniia Rudenko",
+    position: "Junior Front-End Developer",
+    photo: "../../images/rewievs/20.jpg",
+    review:
+      "Тільки днями думала, де мені знайти хороший освітній контент по реакту, а тут ваш сайт 😊 дякую!",
+  },
+  {
+    name: "Andrii Kyselov",
+    position: "Junior Front-End Developer",
+    photo: "../../images/rewievs/21.jpg",
+    review:
+      "Круто! Сайт чіпляє. Додав до вкладок 😉 ",
+  },
+  {
+    name: "Nadiia Bila",
+    position: "Data analyst",
+    photo: "../../images/rewievs/22.jpg",
+    review:
+      "Платформа - супер! Дякую)",
+  },
+  {
+    name: "Sofiia Matsyshena",
+    position: "Front-End Developer",
+    photo: "../../images/rewievs/23.jpg",
+    review:
+      "Вау, це дуже круто! Робота й справді гідна🤌🏻",
+  },
+  {
+    name: "Yevhenii Skubenyk",
+    position: "Front-End Developer",
+    photo: "../../images/rewievs/24.jpg",
+    review:
+      "Супер! Так тримати. Дуже корисний ресурс!",
+  },
+  {
+    name: "Andrii Stepaniuk",
+    position: "Managing Partner",
+    photo: "../../images/rewievs/25.jpg",
+    review:
+      "Круто - так тримати!",
+  },
+  {
+    name: "Лина Курта",
+    position: "Инженер",
+    photo: "../../images/rewievs/26.jpg",
+    review:
+      "Наші вітання..! Успіхів ! 🤝✊💙💛🇺🇦",
+  },
+  {
+    name: "Pavló Valor",
+    position: "Chief Technology Officer",
+    photo: "../../images/rewievs/27.jpg",
+    review:
+      "Як казала одна відома людина: Щоб українізація прошла успішно, ми повинні все зас*ати українською :)",
+  },
+  {
+    name: "Тетяна Л.",
+    position: "Вивчаю напрямок ІТ",
+    photo: "../../images/rewievs/28.jpg",
+    review:
+      "Хороша робота.",
+  },
+  {
+    name: "Dmytro Nevoit",
+    position: "Python Developer",
+    photo: "../../images/rewievs/29.jpg",
+    review:
+      "Дуже круто Вітаю 💪💪💪",
+  },
+  {
+    name: "Svitlana Marianchuk",
+    position: "Talent Sourcing Consultant",
+    photo: "../../images/rewievs/30.jpg",
+    review:
+      "Класна ініціатива!!",
+  },
+  {
+    name: "Yevheniia Melnychenko",
+    position: "Junior Full stack developer",
+    photo: "../../images/rewievs/31.jpg",
+    review:
+      "Це просто неймовірний проєкт. 👏👏👏",
+  },
+  {
+    name: "Andriy Sushko",
+    position: "full-stack developer",
+    photo: "../../images/rewievs/32.jpg",
+    review:
+      "Цікавий ресурс! Дякую, буду користуватись.",
+  },
+  {
+    name: "Inna Mykytiuk",
+    position: "Junior Front-end Developer",
+    photo: "../../images/rewievs/33.jpg",
+    review:
+      "Радісно споглядати, як платформа розвивається і об'єднує спеціалістів різного профілю ❤️ ",
+  },
   // {
   //   name: "ІМЯ",
   //   position: "ПОСАДА",
-  //   photo: "../../images/rewievs/5.JPG",
+  //   photo: "../../images/rewievs/5.jpg",
   //   review:
   //     "ВІДГУК",
   // },
