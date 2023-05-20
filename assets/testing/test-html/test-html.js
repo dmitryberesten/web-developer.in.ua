dc37e22a581e134d8e925b86088a8ef4bd915e7b
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-05-20 23:50:15 +0300

    Додає тестування

--- assets/testing/test-html/test-html.js
+++ assets/testing/test-html/test-html.js
@@ -8,10 +8,18 @@ function createQuiz() {
     li.innerHTML = `
       <p class="form-question">Питання ${i + 1}: ${quiz[i].question}</p>
       <ul>
-        <li><label><input type="radio" name="question${i}" value="${quiz[i].choices[0]}">${quiz[i].choices[0]}</label></li>
-        <li><label><input type="radio" name="question${i}" value="${quiz[i].choices[1]}">${quiz[i].choices[1]}</label></li>
-        <li><label><input type="radio" name="question${i}" value="${quiz[i].choices[2]}">${quiz[i].choices[2]}</label></li>
-        <li><label><input type="radio" name="question${i}" value="${quiz[i].choices[3]}">${quiz[i].choices[3]}</label></li>
+        <li><label><input type="radio" name="question${i}" value="${
+      quiz[i].choices[0]
+    }">${quiz[i].choices[0]}</label></li>
+        <li><label><input type="radio" name="question${i}" value="${
+      quiz[i].choices[1]
+    }">${quiz[i].choices[1]}</label></li>
+        <li><label><input type="radio" name="question${i}" value="${
+      quiz[i].choices[2]
+    }">${quiz[i].choices[2]}</label></li>
+        <li><label><input type="radio" name="question${i}" value="${
+      quiz[i].choices[3]
+    }">${quiz[i].choices[3]}</label></li>
       </ul>
     `;
     quizList.appendChild(li);
@@ -42,9 +50,64 @@ function checkAnswers(event) {
   const percentage = (score / quiz.length) * 100;
   const message =
     percentage >= 70
-      ? "Молодець! Успішне виконання. Залиш заявку на отрмання сертифікату."
+      ? "Молодець! Успішне виконання. Залиш заявку на отримання сертифікату."
       : "Потрібно більше практики. Спробуйте ще раз.";
+
+  if (percentage >= 70) {
+    const userName = prompt("Будь ласка, введіть ваше ім'я:");
+    generateCertificate(userName, score, quiz.length);
+  }
+
   results.innerHTML = `<p>Ваш результат: ${score} з ${quiz.length}. ${message}</p>`;
+}
+
+function generateCertificate(userName, score, totalQuestions) {
+  const currentDate = new Date().toLocaleDateString("en-US");
+
+  const docDefinition = {
+    pageSize: "A4",
+    content: [
+      { text: "Certificate of Achievement", style: "header" },
+      {
+        text: `This document acknowledges that: ${userName}`,
+        style: "subheader",
+      },
+      { text: `Has successfully passed the test: HTML`, style: "subheader" },
+      {
+        text: `Score percentage: ${Math.round(
+          (score / totalQuestions) * 100
+        )}%`,
+        style: "subheader",
+      },
+      { text: `Date: ${currentDate}`, style: "subheader" },
+      {
+        text: "Testing on education platform web-developer.in.ua",
+        style: "footer",
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 22,
+        bold: true,
+        alignment: "center",
+        margin: [0, 0, 0, 220],
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        alignment: "center",
+        margin: [0, 0, 0, 10],
+      },
+      footer: {
+        fontSize: 16,
+        bold: true,
+        alignment: "center",
+        margin: [0, 320, 0, 0],
+      },
+    },
+  };
+
+  pdfMake.createPdf(docDefinition).download("certificate.pdf");
 }
 
 quizForm.addEventListener("submit", checkAnswers);
