cad538beab24a9ca107affde633351d5d51662d0
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-05-23 21:44:25 +0300

    додає нове

---
+++ assets/testing/test-css/test-css-data.js
@@ -0,0 +1,18 @@
+const quiz = [
+  {
+    question: "Який атрибут використовується для задання адреси зображення?",
+    choices: ["alt", "src", "href", "title"],
+    answer: "src",
+  },
+  {
+    question: "Які є типи даних у JavaScript?",
+    choices: [
+      "number, string, boolean, undefined, object",
+      "integer, float, double, string, boolean",
+      "string, boolean, null, array, object",
+      "string, number, boolean, null, undefined",
+    ],
+    answer: "string, number, boolean, null, undefined",
+  },
+  // Додайте сюди ще питання
+];
