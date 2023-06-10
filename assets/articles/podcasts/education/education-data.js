6dbb463a8def4d7d81d3ef82ce3c25f341034cfc
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-06-10 23:59:31 +0300

    Додає подкаст

---
+++ assets/articles/podcasts/education/education-data.js
@@ -0,0 +1,9 @@
+const list = [
+  {
+    id: 1,
+    url: "./education.mp3",
+    author: "Подкаст про IT",
+    title: "Освіта в Україні таки досягла дна. Що робити?",
+    cover: "../player/podcast.png",
+  },
+];
