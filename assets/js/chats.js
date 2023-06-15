6bec05db0458bee8653c2bc29ae312d85a993010
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-06-15 20:01:52 +0300

    Додає чати

--- assets/js/chats.js
+++ assets/js/chats.js
@@ -29,6 +29,41 @@ const chats = [
     name: "Nightly IT ✙ #УкрТг",
     link: "https://t.me/itcrowdua",
   },
+  {
+    logo: "../../images/chats/7.jpg",
+    name: "JavaScript.Ninja",
+    link: "https://t.me/javascript_ninja",
+  },
+  {
+    logo: "../../images/chats/8.jpg",
+    name: "JS різноробочий | FullStack JavaScript",
+    link: "https://t.me/fullstack_devs",
+  },
+  {
+    logo: "../../images/chats/9.jpg",
+    name: "Chat по UpWork",
+    link: "https://t.me/+QZ-O910Qj7NkYTli",
+  },
+  {
+    logo: "../../images/chats/10.jpg",
+    name: "DSC NAU chat",
+    link: "https://t.me/dsc_nau_chat",
+  },
+  {
+    logo: "../../images/chats/11.jpg",
+    name: "Чат - Front-end Бригада",
+    link: "https://t.me/+BlHa2up54Kw1ZWUy",
+  },
+  {
+    logo: "../../images/chats/12.jpg",
+    name: "Code Ukraine // Програмування Chat",
+    link: "https://t.me/itua_chat",
+  },
+  // {
+  //   logo: "../../images/chats/.jpg",
+  //   name: "",
+  //   link: "",
+  // },
 ];
 
 
