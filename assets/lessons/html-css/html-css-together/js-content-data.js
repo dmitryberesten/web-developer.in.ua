c07493edf99c7e96e2a1dec541c5087e5a45f87d
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-07-11 13:38:07 +0300

    Додає уроки

---
+++ assets/lessons/html-css/html-css-together/js-content-data.js
@@ -0,0 +1,7 @@
+const posts = [
+  {
+    title: "1_1 Інтернет, сайти - декілька слів з історії.",
+    imageSrc: "./preview/1.jpg",
+    link: "./1.html",
+  },
+];
---
+++ assets/lessons/html-css/html-css-together/js-pages-data.js
@@ -0,0 +1,9 @@
+var pagesData = [
+  {
+    title: "1_1 Інтернет, сайти - декілька слів з історії.",
+    videoSrc: "https://www.youtube.com/embed/gXhuR06X1jk",
+    description: `Курс "Frontend розробка - найкраща теорія, то є практика" записано у 2021 році як закритий курс для певної групи учнів. `,
+    author: "Навчаємося Разом",
+    authorUrl: "https://www.youtube.com/@itmentor",
+  },
+];
