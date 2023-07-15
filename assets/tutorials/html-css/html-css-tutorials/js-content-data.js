987ab012291f11e36ebc6c2168ec2d8599c88f04
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-07-15 21:50:55 +0300

    Додає туторіали

---
+++ assets/tutorials/html-css/html-css-tutorials/js-content-data.js
@@ -0,0 +1,8 @@
+const posts = [
+  {
+    title:
+      "Адаптивна верстка сайту на базі шаблону ЧФ3. Пояснення дій. HTML CSS JS FIGMA. Частина №1",
+    imageSrc: "./preview/1.jpg",
+    link: "./1.html",
+  },
+];
---
+++ assets/tutorials/html-css/html-css-tutorials/js-pages-data.js
@@ -0,0 +1,10 @@
+var pagesData = [
+  {
+    title:
+      "Адаптивна верстка сайту на базі шаблону ЧФ3. Пояснення дій. HTML CSS JS FIGMA. Частина №1",
+    videoSrc: "https://www.youtube.com/embed/BeG9Bg6Q_30",
+    description: `Адаптивна верстка сайту на базі шаблону Чертоги Фрілансера 3. Пояснення дій. HTML CSS JS FIGMA `,
+    author: "Фрілансер по життю",
+    authorUrl: "https://www.youtube.com/@FreelancerLifeStyle",
+  },
+];
