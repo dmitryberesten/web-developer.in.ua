4ba2ecc3465b4f8a679e53801f0605c059c0746a
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-07-19 15:45:25 +0300

    Додає урок

---
+++ assets/lessons/js/js-zero/js-content-data.js
@@ -0,0 +1,7 @@
+const posts = [
+  {
+    title: "#1 JavaScript з абсолютного нуля. Змінні, вивід на екран",
+    imageSrc: "./preview/1.jpg",
+    link: "./1.html",
+  },
+];
---
+++ assets/lessons/js/js-zero/js-pages-data.js
@@ -0,0 +1,9 @@
+var pagesData = [
+  {
+    title: "#1 JavaScript з абсолютного нуля. Змінні, вивід на екран",
+    videoSrc: "https://www.youtube.com/embed/gDBxIwZBESw",
+    description: `Перша частина курсу для тих, хто ніколи не програмував та хоче навчитися JS`,
+    author: "Програмування Українською",
+    authorUrl: "https://www.youtube.com/@uacoding",
+  },
+];
