e76a868bb1beea331a2de1bf390c505cb4b31664
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-07-19 20:47:56 +0200

    Додав плейліст з ютюб каналу "Програмування Українською"
    Додав 2й, 3й і 4й відео уроки з плейліста "JavaScript з абсолютного нуля"

--- assets/lessons/js/js-zero/js-content-data.js
+++ assets/lessons/js/js-zero/js-content-data.js
@@ -4,4 +4,19 @@ const posts = [
     imageSrc: "./preview/1.jpg",
     link: "./1.html",
   },
+  {
+    title: "#2 JavaScript з абсолютного нуля. Оператори. Робота зі змінними.",
+    imageSrc: "./preview/2.jpg",
+    link: "./2.html",
+  },
+  {
+    title: "#3 JavaScript з абсолютного нуля. Умови. Цикли",
+    imageSrc: "./preview/3.jpg",
+    link: "./3.html",
+  },
+  {
+    title: "#4 JavaScript з абсолютного нуля. Як написати функцію",
+    imageSrc: "./preview/4.jpg",
+    link: "./4.html",
+  },
 ];
