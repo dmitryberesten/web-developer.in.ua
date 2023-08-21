0ee5cb88ca6fb1591b38dbb1c51e01b3eaac3b3a
Dima <dmitryberesten@gmail.com>
2023-08-21 12:25:11 +0300

    Додає подкаст

---
+++ assets/articles/podcasts/interview/interview-data.js
@@ -0,0 +1,9 @@
+const list = [
+  {
+    id: 1,
+    url: "./Коли пора йти на співбесіду.mp3",
+    author: "Подкаст про IT",
+    title: "Коли пора йти на співбесіду?",
+    cover: "../player/podcast.png",
+  },
+];
